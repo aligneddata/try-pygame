3c69041e5a13a7df01c8acd5a414bdeadcef7fdf
aligneddata <aligneddata@gmail.com>
2024-03-15 22:02:06 -0700

    GA as a working game

--- airdefense.py
+++ airdefense.py
@@ -1,5 +1,12 @@
 import pygame
 import datetime
+import random
+
+
+def get_diff_in_seconds(dt1, dt2):
+    dt_delta = dt2 - dt1
+    return dt_delta.seconds + dt_delta.microseconds / 10**6
+
 
 class Global:
     screen_width = 800
@@ -7,76 +14,124 @@ class Global:
     
     
 class Bomb:
-    radius = 15
+    radius = 20
     color = (255, 0, 0)
-    speed = 0.5  # number of pixels per 1000ms
+    speed = .3  # number of pixels per 1000ms
     
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.created_at = datetime.datetime.now()
+        self.annihilated = False
     
     def adjust_position(self):
         new_time = datetime.datetime.now()
-        time_diff = new_time - self.created_at
-        self.y += Bomb.speed * (time_diff.total_seconds() * 1000) / 1000
+        self.y += Bomb.speed * get_diff_in_seconds(self.created_at, new_time)
+        #print(f"Bomb position: {str(new_time)} x={self.x} y={self.y} an={self.annihilated}")
               
+    def in_screen(self):
+        if self.y - Bomb.radius > Global.screen_height:  # upper point is lower than the screen bottom
+            return False
+        if self.y + Bomb.radius < 0:  # bottom point is higher than the screen top
+            return False
+        return True
+    
     def draw(self, screen):
-        self.adjust_position()
-        pygame.draw.circle(screen, Bomb.color, (self.x, self.y), self.radius)
+        if self.in_screen() and not self.annihilated:
+            pygame.draw.circle(screen, Bomb.color, (self.x, self.y), self.radius)
+            self.adjust_position()
 
 
-class BombList: 
+class BombList:
+    bomb_creation_speed = .4  # number of new bombs per second
+    
     def __init__(self):
         self.bombs = []
         bomb1 = Bomb(250, 40)
-        bomb2 = Bomb(300, 50)
         self.bombs.append(bomb1)
-        self.bombs.append(bomb2)
+        self.created_at = datetime.datetime.now()
         
     def draw(self, screen):
         for bomb in self.bombs:
             bomb.draw(screen)
+
+     
+    def generate_bomb(self):
+        new_time = datetime.datetime.now()
+        expected_number = get_diff_in_seconds(self.created_at, new_time) * BombList.bomb_creation_speed
+        #print(f"Expected {expected_number} Actual {len(self.bombs)}")
+        for i in range(int(expected_number) - len(self.bombs)):
+            x = random.randint(Bomb.radius, Global.screen_width - Bomb.radius)
+            y = 1 + 0 - Bomb.radius
+            bomb = Bomb(x, y)
+            self.bombs.append(bomb)
+            print("Created a new bomb")
+
+    def test_if_game_over(self):
+        for bomb in self.bombs:
+            if not bomb.annihilated and bomb.y+Bomb.radius >= Global.screen_height:
+                #print("Game over!")
+                return True
 
 
 class Shell:
     length = 30
     width = 5
     color = (0, 0, 255)
-    speed = 1  # number of pixels per 1000ms
+    speed = 2.  # number of pixels per second
     
     def __init__(self, x, y):
         self.x = x
-        self.y = y        
+        self.y = y 
+        self.y2 = self.y + Shell.length       
         self.created_at = datetime.datetime.now()
+        self.annihilated = False
     
     def adjust_position(self):
         new_time = datetime.datetime.now()
-        time_diff = new_time - self.created_at
-        self.y -= Bomb.speed * (time_diff.total_seconds() * 1000) / 1000
-
+        self.y -= Shell.speed * get_diff_in_seconds(self.created_at, new_time)
+        self.y2 = self.y + Shell.length
+        #print(f"Shell position: {str(new_time)} x={self.x} y={self.y} y2={self.y2} an={self.annihilated}")
+     
+    def in_screen(self):
+        if self.y > Global.screen_height:  # upper point is lower than the screen bottom
+            return False
+        if self.y2 < 0:  # bottom point is higher than the screen top
+            return False
+        return True
+    
     def draw(self, screen):
-        self.adjust_position()
-        pygame.draw.line(screen, 
-                         Shell.color, 
-                         (self.x, self.y), 
-                         (self.x, self.y+Shell.length), 
-                         Shell.width)
+        if self.in_screen() and not self.annihilated:
+            pygame.draw.line(screen, 
+                            Shell.color, 
+                            (self.x, self.y), 
+                            (self.x, self.y2), 
+                            Shell.width)
+            self.adjust_position()
 
+    
         
 class ShellList:   
     def __init__(self):
         self.shells = []
-        shell1 = Shell(0,0)
-        shell2 = Shell(100, 10)
-        shell3 = Shell(200, 20)
-        self.shells.append(shell1)
-        self.shells.append(shell2)
-        self.shells.append(shell3)
     
     def draw(self, screen):
         for shell in self.shells:
            shell.draw(screen)
+
+    # tell if this shell is currently hitting the bomb
+    # if the shell is not even in the range of the bomb, fail.
+    # if in the range of x, and the shell's head enters into the domain of bomb y range, success.
+    def test_if_hitting_target(self, bombList: BombList):
+        for shell_id in range(len(self.shells)):
+            for bomb_id in range(len(bombList.bombs)):
+                shell = self.shells[shell_id]
+                bomb = bombList.bombs[bomb_id]
+                if shell.in_screen() and not shell.annihilated and bomb.in_screen() and not bomb.annihilated:
+                    if shell.x >= (bomb.x - Bomb.radius) and shell.x <= (bomb.x + Bomb.radius) and shell.y >= (bomb.y - Bomb.radius) and shell.y <= (bomb.y + Bomb.radius):
+                        shell.annihilated = True
+                        bomb.annihilated = True
+                        print("Target hit!")
 
 
 class Cannon:
@@ -86,10 +141,23 @@ class Cannon:
     step_size = 2
     
 
+class Text:
+    def __init__(self, message):
+        self.message = message
+    
+    def draw(self, screen):
+        TEXT_COLOR = (255, 255, 255)
+        BACKGROUND_COLOR = (0, 0, 0)
+        font = pygame.font.SysFont(None, 48) 
+        text = font.render(self.message, True, TEXT_COLOR)
+        screen.fill(BACKGROUND_COLOR)
+        screen.blit(text, (Global.screen_width/2 - 100, Global.screen_height/2)) 
+        
+    
 def main():
     pygame.init()
     screen = pygame.display.set_mode((Global.screen_width, Global.screen_height))
-    pygame.display.set_caption('Demo')
+    pygame.display.set_caption('Air Defense')
 
     cannon = pygame.Rect((0, Global.screen_height - Cannon.height - 1, Cannon.width, Cannon.height))
     shell_list = ShellList()
@@ -99,6 +167,10 @@ def main():
     while not game_over:  # game loop        
         screen.fill((0, 0, 0))
         
+
+        bomb_list.generate_bomb()
+        shell_list.test_if_hitting_target(bomb_list)
+
         pygame.draw.rect(screen, Cannon.color, cannon)
         shell_list.draw(screen)
         bomb_list.draw(screen)
@@ -109,18 +181,25 @@ def main():
             cannon.move_ip(-1*Cannon.step_size, 0)
         elif key[pygame.K_RIGHT] == True and cannon.right < Global.screen_width-1:
             cannon.move_ip(1*Cannon.step_size, 0)
-        # fire cannon
-        elif key[pygame.K_SPACE] == True:
-            shell = Shell(cannon.left + Cannon.width/2, cannon.top - Shell.length)
-            shell_list.shells.append(shell)
         
         # event processing
         for event in pygame.event.get():  # event handler
             if event.type == pygame.QUIT:
                 game_over = True
-        
+            if event.type == pygame.KEYUP:
+                key = event.key
+                # fire cannon
+                if key == pygame.K_SPACE:
+                    print("Firing a shell")
+                    shell = Shell(cannon.left + Cannon.width/2, cannon.top - Shell.length)
+                    shell_list.shells.append(shell)
+                
+        # is game over 
+        if bomb_list.test_if_game_over():
+            Text("Game Over!").draw(screen)
+
         # refresh screen
-        pygame.display.update()
+        pygame.display.update()        
          
     pygame.quit()  # clean
 
